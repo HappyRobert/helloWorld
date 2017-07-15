26b0a4100c1d8d534f5c75a4ef6a480129e3afe4
wangxu <wangxu35@meituan.com>
2017-07-15 23:30:46 +0800

    first test

---
+++ src/java/testAboutStart/HelloWorld.java
@@ -0,0 +1,10 @@
+package testAboutStart;
+
+/**
+ * Created by wangxu on 2017/7/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!");
+    }
+}
